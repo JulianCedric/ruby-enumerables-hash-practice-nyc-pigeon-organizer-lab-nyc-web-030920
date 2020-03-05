a2d734ca9dd2314e403cbde9901c9713d26b8ed5
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-05 22:47:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -53,9 +53,11 @@ end
 def nyc_pigeon_organizer(pigeon_hashes)
   pigeons_by_name = [] 
   pigeon_hashes.each do |attributes, attributes_details_hash|
-    attributes_details_hash.each do |attributes_details_hash, names_array|
+    attributes_details_hash.each do |new_value, names_array| 
+      names_array.each do |name| 
       binding.pry 
       pigeons_by_name << names_array
+    end 
     end 
   end
 
