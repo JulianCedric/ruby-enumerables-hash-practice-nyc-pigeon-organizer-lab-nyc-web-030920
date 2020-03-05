8cc9a9e1ee40870d53da8b16a2a156ae416be0bc
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-05 21:06:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,84 @@
+ def flatten_a_o_a(aoa)
+  result = []
+  i = 0
+  while i < aoa.length do
+    k = 0
+    while k < aoa[i].length do
+      result << aoa[i][k]
+      k += 1
+    end
+    i += 1
+  end
+  result
+end 
+ 
+names1 = ["Theo", "Peter Jr.", "Lucky", "Ms .K", "Queenie", "Andrew", "Alex"]
+names2 = ["Luca", "Lola"]
+ 
+  pigeon_data1 = 
+      { 
+      :color => 
+          {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+          },
+      :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+          },
+      :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+          }
+      }
+
+  pigeon_data2 = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+  
+def nyc_pigeon_organizer(pigeon_hashes)
+pigeons_by_name = [] 
+pigeon_hashes.each do |attributes, attributes_details_hash|
+  attributes_details_hash.each do |attributes_details_hash, names_array|
+    pigeons_by_name << names_array
+  end 
+end
+(flatten_a_o_a(pigeons_by_name)).uniq 
+# pigeons_by_name
+end 
+
+p nyc_pigeon_organizer(pigeon_data2) 
+
+
+
+
+
+  # attributes = { :color => [], :gender => [], :lives => [] }
+
+  # hash = { names[0] => attributes } 
+  
+  # hash_index = 0 
+  # while hash_index < names.length do 
+  #   hash[names[hash_index]] = attributes
+  #   hash_index += 1 
+  # end
+
+
 
 puts ""
 puts "" 
